14e54a5789e0eeaf43ddb2b3ca8047faa1f192d7
Abhishek Gaur <abhibhardwaj@192.168.1.4>
2026-05-31 17:33:48 +0530

    updated code

--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
+      family: 4 // Force IPv4 to prevent Render from routing over unsupported IPv6 towards Atlas Free Tier
     });
     console.log('MongoDB connected');
   } catch (err) {
